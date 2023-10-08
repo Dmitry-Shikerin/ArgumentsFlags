b1815c91b0ef745b3cfd27bb26718d1bbb5a098b
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-10-08 15:09:47 +0300

    Добавьте файлы проекта.

---
+++ Аргументы флаги это плохо/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Аргументы_флаги_это_плохо
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void OnEnable()
+        {
+            _effects.StartEnableAnimation();
+        }
+
+        public void OnDisable() 
+        {
+            _pool.Free(this);
+        }
+    }
+}
